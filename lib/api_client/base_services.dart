b6e3cbbe470eff431e0bab6427c0d9d9e2a4acd9
jamshaid Malik <alimyasir5@gmail.com>
2024-07-25 11:07:40 +0500

    run on ios and modified api client

--- lib/api_client/base_services.dart
+++ lib/api_client/base_services.dart
@@ -1,47 +1,89 @@
- import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart'as getx ;
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart' as getx;
 import 'package:touch_down/api_client/api_routes.dart';
 import 'package:touch_down/utils/local_storage.dart';
 
- class BaseServices extends getx.GetxService  {
-   final Dio _dio = Dio(BaseOptions(baseUrl: ApiRoutes.baseUrl, headers: {
-       'Accept': 'application/json',
-       'Authorization': 'Bearer ${LocalStorage.instance.bearerToken}',
-     },
-   ));
+class BaseServices {
+  final Dio _dio;
 
-   Future<dynamic> apiCall(String method, String endpoint, {dynamic data}) async {
-     try {
-       debugPrint('API Call: $method ${ApiRoutes.baseUrl + endpoint} \nHeaders: ${_dio.options.headers} \nBody: $data');
-       String completeUrl = ApiRoutes.baseUrl + endpoint;
-        Response response ;
+  BaseServices()
+      : _dio = Dio(
+    BaseOptions(
+      baseUrl: ApiRoutes.baseUrl,
+    ),
+  );
 
-       switch (method.toUpperCase()) {
-         case 'GET':
-           response = await _dio.get(completeUrl);
-           break;
-         case 'POST':
-           response = await _dio.post(completeUrl, data: data);
-           break;
-         case 'PUT':
-           response = await _dio.put(completeUrl, data: data);
-           break;
-         case 'DELETE':
-           response = await _dio.delete(completeUrl);
-           break;
-         default:
-           throw Exception('Unsupported HTTP method: $method');
-       }
+   Future<Response?> apiCall(
+      String method,
+      String endpoint, {
+        dynamic data,
+      }) async {
+    final headers = await _buildHeaders();
+    final url = ApiRoutes.baseUrl + endpoint + (method.toUpperCase() == 'GET' ? _buildQueryString(data) : '');
 
-       debugPrint('API Response: ${response.data}');
-       return response.data;
-     } catch (e) {
-       // Handle error
-       debugPrint('Error occurred during API call: $e');
-       return null;
-     }
-   }
+    try {
+      debugPrint('''
+╔═══════════════════════════════════════════════════╗
+║                   API Call Details                ║
+╟───────────────────────────────────────────────────╢
+║ URL: $url
+║ Method: $method
+║ Headers: $headers
+║ Body: $data
+╚═══════════════════════════════════════════════════╝
+''');
 
+      final response = await _dio.request(
+        url,
+        data: method.toUpperCase() == 'GET' ? null : data,
+        queryParameters: method.toUpperCase() == 'GET' ? data : null,
+        options: Options(method: method, headers: headers),
+      );
 
- }
+      return response;
+    } on DioException catch (e) {
+      debugPrint('''
+╔═══════════════════════════════════════════════════╗
+║                   API Call Error                  ║
+╟───────────────────────────────────────────────────╢
+║ URL: $url
+║ Error: ${e.message}
+║ Response: ${e.response?.data ?? 'No Response Data'}
+╚═══════════════════════════════════════════════════╝
+''');
+      return e.response;
+    } catch (e) {
+      debugPrint('''
+╔═══════════════════════════════════════════════════╗
+║                Unexpected Error                   ║
+╟───────────────────────────────────────────────────╢
+║ URL: $url
+║ Error: $e
+╚═══════════════════════════════════════════════════╝
+''');
+      return null;
+    }
+  }
+
+  Future<Map<String, String>> _buildHeaders() async {
+    final headers = {'Accept': 'application/json'};
+    final bearerToken = LocalStorage.instance.bearerToken;
+
+    if (bearerToken.isNotEmpty) {
+      headers['Authorization'] = 'Bearer $bearerToken';
+    }
+    return headers;
+  }
+
+  String _buildQueryString(Map<String, dynamic>? params) {
+    if (params == null || params.isEmpty) return '';
+
+    final queryString = StringBuffer('?');
+    params.forEach((key, value) {
+      queryString.write('$key=$value&');
+    });
+
+    return queryString.toString().substring(0, queryString.length - 1); // Remove trailing '&'
+  }
+}
--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -7,54 +7,75 @@ import 'package:touch_down/utils/local_storage.dart';
 import 'package:touch_down/view/nav_bar/navigation_menu.dart';
 import 'package:touch_down/widgets/k_snack_bar.dart';
 
-class AuthController extends GetxController{
+class AuthController extends GetxController {
   BaseServices baseServices = BaseServices();
- TextEditingController nameController= TextEditingController(text: 'Sultan Ali');
- TextEditingController emailController= TextEditingController(text: 'sultanali@gmail.com');
- TextEditingController passwordController= TextEditingController(text: '12345678@');
- TextEditingController phoneNumberController= TextEditingController();
- RxBool showPassword=true.obs;
- RxBool confirmPassword=false.obs;
+  TextEditingController nameController =
+      TextEditingController(text: 'Sultan Ali');
+  TextEditingController emailController =
+      TextEditingController(text: 'sultanali@gmail.com');
+  TextEditingController passwordController =
+      TextEditingController(text: '12345678@');
+  TextEditingController phoneNumberController = TextEditingController();
+  RxBool showPassword = true.obs;
+  RxBool confirmPassword = false.obs;
 
+  void register() async {
+    try{
+      final response =
+      await baseServices.apiCall('post', ApiRoutes.register, data: {
+        'email': emailController.text.trim(),
+        'password': passwordController.text.trim(),
+        'name': nameController.text.trim(),
+        'phone': phoneNumberController.text.trim(),
+      });
+      debugPrint('Register Response: $response');
+      if (response?.statusCode == 200) {
+        var data = json.decode(response!.data);
+        LocalStorage.instance.setBearerToken(data['data']['token']);
+        showSnackBar('Success', data['message'].toString());
+        await Future.delayed(const Duration(seconds: 2));
+        Get.to(() => CustomBottomBar());
+      }
+    } catch (e) {
+      showSnackBar('Error', e.toString());
+    }
+  }
 
+  bool isValidPassword(String password) {
+    String pattern = r'^(?=.*?[#?!@$%^&*-]).{8,}$';
+    RegExp regex = RegExp(pattern);
+    return regex.hasMatch(password);
+  }
 
- void register() async{
-final response= await baseServices.apiCall('put',ApiRoutes.register,data: {
- 'email' : emailController.text.trim(),
- 'password': passwordController.text.trim(),
- 'fullname': nameController.text.trim(),
- 'phone': phoneNumberController.text.trim(),
- });
-   if(response.statusCode==200){
-     var data= jsonDecode(response.body);
-     LocalStorage.instance.setBearerToken(data['data']['token']);
-     showSnackBar('Success', data['message'].toString());
-     await Future.delayed(const Duration(seconds: 2));
-     Get.to(()=>CustomBottomBar());
-   }else{
-     var data = jsonDecode(response.body);
-     showSnackBar('Error', data['message'].toString());
-   }
- }
+  bool isValidEmail(String email) {
+    String pattern = r'^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]+';
+    RegExp regex = RegExp(pattern);
+    return regex.hasMatch(email);
+  }
 
- bool isValidPassword(String password) {
-   String pattern = r'^(?=.*?[#?!@$%^&*-]).{8,}$';
-   RegExp regex = RegExp(pattern);
-   return regex.hasMatch(password);
- }
- bool isValidEmail(String email) {
-   String pattern = r'^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]+';
-   RegExp regex = RegExp(pattern);
-   return regex.hasMatch(email);
- }
- void togglePassword(){
-   showPassword.value = !showPassword.value;
-   update();
- }
- void confirmTogglePassword(){
-   confirmPassword.value = !confirmPassword.value;
-   update();
- }
+  void togglePassword() {
+    showPassword.value = !showPassword.value;
+    update();
+  }
 
+  void confirmTogglePassword() {
+    confirmPassword.value = !confirmPassword.value;
+    update();
+  }
+
+  demoApiCall() async {
+    try {
+      final response = await baseServices.apiCall('GET', 'https://jsonplaceholder.typicode.com/posts');
+      debugPrint('Demo API Response: ${response?.statusCode} ${response?.data}');
+      final data = response!.data;
+      if (response.statusCode == 200) {
+        showSnackBar('Success', data['message']);
+      } else {
+        showSnackBar('Error', data['errorMessage'], isError: true);
+      }
+    } catch (e) {
+      showSnackBar('Error', e.toString(), isError: true);
+    }
+  }
 
 }
